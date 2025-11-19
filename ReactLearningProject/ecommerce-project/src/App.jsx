8c6a66262ab0dfc5cff793f8f3340bca7956df41
Tanmay Patel <tnmypatel174@gmail.com>
2025-11-20 02:13:33 +0530

    Move OrderPage and TrackingPage to react

--- ReactLearningProject/ecommerce-project/src/App.jsx
+++ ReactLearningProject/ecommerce-project/src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes,Route } from 'react-router';
 import CheckoutPage from './Pages/CheckoutPage';
 import './App.css'
+import OrdersPage from './Pages/OrdersPage'
 import Home from './Pages/Home';
+import TrackingPage from './Pages/TrackingPage';
 
 function App() {
   return(
     <Routes>
       <Route index element={<Home />} />
       <Route path="checkout" element={<CheckoutPage />}></Route>
+      <Route path="orders" element={<OrdersPage/>}></Route>
+      <Route path="tracking" element={<TrackingPage />}></Route>
     </Routes>
    
   );
