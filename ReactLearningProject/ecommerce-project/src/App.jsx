4a5806a131781e9e8b486f2ec439bf9c28d3ba99
Tanmay Patel <tnmypatel174@gmail.com>
2025-11-20 01:43:37 +0530

    Created Multiple Pages using react-router

--- ReactLearningProject/ecommerce-project/src/App.jsx
+++ ReactLearningProject/ecommerce-project/src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes,Route } from 'react-router';
 import './App.css'
 import Home from './Pages/Home';
 
 function App() {
   return(
-    <Home/>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="checkout" element={<div>New Route Testing Page</div>}></Route>
+    </Routes>
+   
   );
 }
 
