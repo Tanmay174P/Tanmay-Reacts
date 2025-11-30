2dec36c6f6198ee32d9651313a8d4ab8da0a283b
Tanmay Patel <tnmypatel174@gmail.com>
2025-12-01 01:43:51 +0530

    test clicking add to cart button

--- ReactLearningProject/ecommerce-project/src/Pages/Home/Product.test.jsx
+++ ReactLearningProject/ecommerce-project/src/Pages/Home/Product.test.jsx
@@ -1,11 +1,16 @@
-import { it, expect, describe,vi } from 'vitest';
-import  Product  from './Product';
+import { it, expect, describe, vi, expectTypeOf } from 'vitest';
+import Product from './Product';
+import userEvent from '@testing-library/user-event';
+/* userEvent simulates user actions → means it pretends to act like a real user clicking, typing, tabbing, etc. */
 import { render, screen /* check the fake web page */ } from '@testing-library/react' // render a component for a fake web page.
+import axios from 'axios';
+
+vi.mock('axios'); //mock the entire fake version of axios and will not do anything.
 
 //Testing a Component -> 
 // render(<Product/ >) -> display the component on the page.
 describe('Product Component', () => {
-     it('display the product details correctly', () => {
+    it('display the product details correctly', () => {
         const product = {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -23,7 +28,7 @@ describe('Product Component', () => {
         // Sol. is to make a (mock) -> fake version of the funciton.
 
         render(<Product product={product} loadCart={loadCart} />);
-        
+
         expect(
             screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')
         ).toBeInTheDocument(); // checks if this element exist on the fake web page.
@@ -34,14 +39,44 @@ describe('Product Component', () => {
 
         expect(
             screen.getByTestId('product-image')
-        ).toHaveAttribute('src','images/products/athletic-cotton-socks-6-pairs.jpg')
+        ).toHaveAttribute('src', 'images/products/athletic-cotton-socks-6-pairs.jpg')
 
         expect(
             screen.getByTestId('product-rating-stars-image')
-        ).toHaveAttribute('src','images/ratings/rating-45.png')
+        ).toHaveAttribute('src', 'images/ratings/rating-45.png')
 
         expect(
             screen.getByText('87')
         ).toBeInTheDocument();
     });
-})
+
+    it('adds a products to the cart'), async () => {
+        const product = {
+            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            rating: {
+                stars: 4.5,
+                count: 87
+            },
+            priceCents: 1090,
+            keywords: ["socks", "sports", "apparel"]
+        };
+        const loadCart = vi.fn();
+        render(<Product product={product} loadCart={loadCart} />);
+
+        const user = userEvent.setup();
+        const addToCartButton = screen.getByText('add-to-cart-button');
+        await user.click(addToCartButton);
+
+        expect(axios.post).toHaveBeenCalledWith(
+            '/api/cart-items',
+            {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1
+            }
+        )
+
+        expect(loadCart).toHaveBeenCalled();
+    }
+});
